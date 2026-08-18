8c7dab27675a66093630c9ad273a3ca9e72fea3a
jiahw4ng <jiahw24@yahoo.com>
2026-08-19 01:03:47 +0800

    rename bot and create simple greeting and bye text

---
+++ src/main/java/Martin.java
@@ -0,0 +1,20 @@
+public class Martin {
+    public static void main(String[] args) {
+        String banner = "M   M   A   RRR  TTTTT I N   N\n"
+                + "MM MM  A A  R  R   T   I NN  N\n"
+                + "M M M AAAAA RRR    T   I N N N\n"
+                + "M   M A   A R R    T   I N  NN\n"
+                + "M   M A   A R  R   T   I N   N\n";
+
+        String horizontalLine = "_____________________________________________________";
+        String greeting = "Hello! I'm Martin.\nWhat can I do for you?";
+        String bye = "Bye. Hope to see you again soon!";
+        
+        System.out.println(horizontalLine);
+        System.out.println(banner);
+        System.out.println(greeting);
+        System.out.println(horizontalLine);
+        System.out.println(bye);
+        System.out.println(horizontalLine);
+    }
+}
